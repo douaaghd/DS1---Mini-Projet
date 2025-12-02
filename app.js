1e014f2edc35e9aacff9c0052158070e1bb01c02
douaa <ghidaouidouaa3@gmail.com>
2025-12-02 09:48:22 +0100

    Add user routes and connect them to the app

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/api/users', userRoutes);
+
+
 // Route test
 app.get('/', (req, res) => {
   res.send('API DS1 Projet fonctionne ! ');
