a47b7a3d0f9bd4126a9269f2ae6e0aded3dcb4c2
douaa <ghidaouidouaa3@gmail.com>
2025-12-02 11:51:20 +0100

    Fix(server): configure Express 4 and middleware

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 // Charger les variables d'environnement en premier
 require('dotenv').config();
 
-const app = require('./app');
 const connectDB = require('./config/db');
-
-// Définir le port après avoir chargé dotenv
+//houni app est bien chargee juste le serveur mezel pas demarré encore 
+const app = require('./app');
+// Définir le port ama baed  dotenv chargee
 const PORT = process.env.PORT || 5000;
+//fonction pour demarrer le serveur 
+const startServer = async () => {
+    try {
+        // Tawa nestannew el connexion m3a el base (mongo iwali hather)
+        await connectDB(); 
 
-// Connexion à MongoDB
-connectDB();
-
-// Lancer le serveur
-app.listen(PORT, () => {
-  console.log(`Serveur lancé sur le port ${PORT} `);
-});
-
+        // Ken el connexion shiha, on lance connexion 
+        app.listen(PORT, () => {
+            console.log(`Serveur lancé sur le port ${PORT} `);
+            console.log('MongoDB prêt et serveur en écoute.');
+            // El base tawa 7adhra w el serveur yestanna fi les requêtes.
+        });
+        } catch (error) {
+        // Kén fama ghalta kbira fil démarrage wala connexion
+        console.error("Échec critique lors du démarrage :", error.message);
+        process.exit(1);
+    }
+};
+//execution (bech testanan connexion lel mongodb bech tekhdem grace a await)
+startServer();
